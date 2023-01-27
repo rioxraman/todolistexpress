afaf872eeaa2ec64071a5c410f3a836b3cc8a84f
saibys <rareguy.raman@gmail.com>
2023-01-27 11:18:00 +0530

    views

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname, 'views'))
 
+app.get('/', (req, res) => {
+  res.render('index',{
+    title: "node class"
+  })
+})
+// app.use(path.join(__dirname, 'public'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
